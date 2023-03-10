cc522eafb7eaf3e23d87015a9e40a0b4598577ab
Michiel van der Geest <hello@michielvandergeest.com>
2023-03-10 14:30:29 +0100

    Refactored element factory to return functions by reference.

--- src/element.js
+++ src/element.js
@@ -2,39 +2,40 @@ import threadx from '@lightningjs/threadx'
 
 let counter = 0
 
-export default (config) => {
-  const elementId = ++counter
-  return {
-    populate(data) {
-      config.elementId = elementId
-      threadx.send('bolt', { ...config, ...data })
-    },
-    set(property, value) {
-      if (property === 'imageSource') {
-        if (value !== -1) {
-          threadx.send('images', {
-            id: elementId,
-            value,
-          })
-        }
-      } else if (property === 'text') {
-        threadx.send('text', {
-          elementId,
+const element = {
+  populate(data) {
+    this.config.elementId = this.elementId
+    threadx.send('bolt', { ...this.config, ...data })
+  },
+  set(property, value) {
+    if (property === 'imageSource') {
+      if (value !== -1) {
+        threadx.send('images', {
+          id: this.elementId,
           value,
         })
-      } else {
-        const mutation = {
-          elementId,
-        }
-        mutation[property] = value
-        threadx.send('mutations', mutation)
       }
-    },
-    delete() {
-      // todo
-    },
-    id() {
-      return elementId
-    },
-  }
+    } else if (property === 'text') {
+      threadx.send('text', {
+        elementId: this.elementId,
+        value,
+      })
+    } else {
+      const mutation = {
+        elementId: this.elementId,
+      }
+      mutation[property] = value
+      threadx.send('mutations', mutation)
+    }
+  },
+  delete() {
+    // todo
+  },
+  id() {
+    return this.elementId
+  },
+}
+
+export default (config) => {
+  return { ...element, ...{ elementId: ++counter, config } }
 }
