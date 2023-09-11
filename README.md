556ac0309f4392f197d8b0701d91cd5708842885
Michiel van der Geest <hello@michielvandergeest.com>
2023-09-11 13:04:46 +0200

    Added licenses to the project.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Lightning 3 App Development Framework
+
+#### Code name: Lightning _Bolt_
+
+With the Lightning 3 App Development Framework it becomes a breeze to build great Lightning 3 applications.
+
+The L3 AppDev Framework is built on top of the L3 Rendererer and aims to provide a **great developer experience**, making it fun and easy to build your apps!
+
+Built from the ground up by the LightningJS team, we're offering a **Complete App framework**, full of features to speed up development, while keeping it **lightweight** and focused on **performance**.
+
+The L3 AppDev Framework uses **readable** XML-style template syntax and comes with built-in reactivity.
--- src/lib/codegenerator/generator.test.js
+++ src/lib/codegenerator/generator.test.js
@@ -1,3 +1,22 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2023 Comcast
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import test from 'tape'
 import generator from './generator.js'
 
@@ -81,8 +100,6 @@ test('Generate render and effect code for a template with a single simple elemen
       },
     ],
   }
-
-  // stel, parent is het component en die heeft een key 'element' (die dan weer een id heeft)
 
   const expectedRender = `
   function anonymous(parent,component,context) {
