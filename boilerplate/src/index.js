8391b3d28ab384029865dba8e5fc0488d2d86106
Michiel van der Geest <hello@michielvandergeest.com>
2023-10-04 09:33:46 +0200

    Added first round of type definitions for the main Blits API.

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -3,7 +3,7 @@ import fontLoader from './fontloader.js?importChunkUrl'
 
 import App from './App.js'
 
-const settings = {
+Blits.Launch(App, 'app', {
   w: 1920,
   h: 1080,
   multithreaded: false,
@@ -14,6 +14,4 @@ const settings = {
     {family: 'raleway', type: 'msdf', png: '/fonts/Raleway-ExtraBold.msdf.png', json: '/fonts/Raleway-ExtraBold.msdf.json'},
     {family: 'opensans', type: 'web', file: '/fonts/OpenSans-Medium.ttf'}
   ],
-}
-
-Blits.Launch(App, 'app', settings)
+})
--- boilerplate/src/renderer.js
+++ boilerplate/src/renderer.js
@@ -1,3 +1,4 @@
+// @ts-ignore
 import renderer from '@lightningjs/blits/renderer'
 
 renderer()
