e4eb7f6e86fecf1ab36774897d06343345d99683
Michiel van der Geest <hello@michielvandergeest.com>
2023-11-01 11:40:17 +0100

    Fixed import of default router transition.

--- src/router/router.js
+++ src/router/router.js
@@ -15,7 +15,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { default as fadeInFadeOutTransition } from './transitions/fadeIn.js'
+import { default as fadeInFadeOutTransition } from './transitions/fadeInOut.js'
 
 let currentRoute
 
