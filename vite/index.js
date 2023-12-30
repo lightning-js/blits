27729af4b31b93c193162c247e5b78efe95b7f48
Michiel van der Geest <hello@michielvandergeest.com>
2023-12-30 17:21:59 +0100

    Added precompiler vite plugin. Exported all blits related vite plugins as a single array to simplify App vite configs and make them future proof.

--- vite/index.js
+++ vite/index.js
@@ -1,1 +1,7 @@
+import preCompiler from './preCompiler.js'
+import { importChunkUrl } from '@lightningjs/vite-plugin-import-chunk-url'
+
 export { importChunkUrl as importChunkUrl } from '@lightningjs/vite-plugin-import-chunk-url'
+export { default as preCompiler } from './preCompiler.js'
+
+export default [importChunkUrl(), preCompiler()]
