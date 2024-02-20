a97642c980748c2e9ee1ab094c6910aed6ad922c
Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>
2024-02-20 19:04:42 +0530

    Clean up childrens of old view
    Signed-off-by: Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>

--- src/router/router.js
+++ src/router/router.js
@@ -236,9 +236,11 @@ const removeView = async (route, view, transition) => {
     cacheMap.set(route, { view: view, focus: previousFocus })
   } else {
     // remove and cleanup
-    for (let i = 0; i < view[symbols.children].length - 1; i++) {
+    for (let i = 0; i < view[symbols.children].length; i++) {
       if (view[symbols.children][i] && view[symbols.children][i].destroy) {
         view[symbols.children][i].destroy()
+        view[symbols.children][i] = null
+      } else if (view[symbols.children][i]) {
         view[symbols.children][i] = null
       }
     }
