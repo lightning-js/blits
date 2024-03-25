7453ba23121cf282e60a204a751bf3b8c6e764ca
jfboeve <jeffrey_boeve@comcast.com>
2024-03-25 11:04:57 +0100

    use correct function for adding effects

--- src/shaderLoader.js
+++ src/shaderLoader.js
@@ -1,12 +1,13 @@
-import Settings from '.settings'
+import Settings from './settings'
 import { renderer } from './launch'
 
 export default () => {
+  console.log('load shaders')
   const stage = renderer.driver.stage
   Settings.get('shaders', []).forEach((shader) => {
     stage.shManager.registerShaderType(shader.name, shader.type)
   })
   Settings.get('shaderEffects', []).forEach((effect) => {
-    stage.shManager.registerShaderType(effect.name, effect.type)
+    stage.shManager.registerEffectType(effect.name, effect.type)
   })
 }
