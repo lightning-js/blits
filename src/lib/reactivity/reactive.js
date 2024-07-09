788952003abf20c8027378db2c73e05a4e92d0d0
Michiel van der Geest <hello@michielvandergeest.com>
2024-07-09 15:04:53 +0200

    Added check to prevent ImageTextures to be made reactive.

--- src/lib/reactivity/reactive.js
+++ src/lib/reactivity/reactive.js
@@ -32,8 +32,12 @@ const getRaw = (value) => {
 }
 
 const reactiveProxy = (original, _parent = null, _key) => {
-  // don't create a proxy when a Blits component is assigned to a state variable
-  if (typeof original === 'object' && original[symbols.id]) {
+  // don't create a proxy when a Blits component or an Image Texture
+  // is assigned to a state variable
+  if (
+    (typeof original === 'object' && original[symbols.id]) ||
+    original.constructor.name === '_ImageTexture'
+  ) {
     return original
   }
 
