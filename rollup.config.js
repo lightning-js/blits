07402e342b92ac583b8292349ff96307dad4296b
jfboeve <jeffrey_boeve@comcast.com>
2024-07-15 11:29:49 +0200

    add rollup config + dist to test

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
+import { resolve as pathResolve } from 'node:path'
+
+export default {
+  input: './index.js',
+  output: {
+    file: pathResolve('dist/blits.min.js'),
+    format: 'esm',
+    sourcemap: true,
+  },
+  onwarn: (msg, warn) => {
+    if (msg.code !== 'CIRCULAR_DEPENDENCY') {
+      warn(msg)
+    }
+  },
+  plugins: [resolve(), commonjs(), terser()],
+  external: ['execa', 'kolorist', 'ora', 'prompts', 'replace-in-file', 'validate-npm-package-name'],
+}
