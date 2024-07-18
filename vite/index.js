34bca43a9d02e137e0ca5a6397ed612f5688c45a
jfboeve <jeffrey_boeve@comcast.com>
2024-07-18 14:16:08 +0200

    remove importChunkUrl plugin/dependency

--- vite/index.js
+++ vite/index.js
@@ -15,13 +15,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { importChunkUrl } from '@lightningjs/vite-plugin-import-chunk-url'
-
 import preCompiler from './preCompiler.js'
 import msdfGenerator from './msdfGenerator.js'
 
-export { importChunkUrl as importChunkUrl } from '@lightningjs/vite-plugin-import-chunk-url'
 export { default as preCompiler } from './preCompiler.js'
 export { default as msdfGenerator } from './msdfGenerator.js'
 
-export default [importChunkUrl(), preCompiler(), msdfGenerator()]
+export default [preCompiler(), msdfGenerator()]
