14b3adf44657af0fa0dfbffdb1adefe6849cbb3f
Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>
2024-08-13 16:01:35 +0530

    Fixes default key in for-loop on deletion
    Signed-off-by: Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>

--- src/lib/codegenerator/generator.js
+++ src/lib/codegenerator/generator.js
@@ -310,7 +310,7 @@ const generateForLoopCode = function (templateObject, parent) {
       let l = rawCollection.length
       while(l--) {
         const ${item} = rawCollection[l]
-        keys.add('' +  ${interpolate(key, '')})
+        keys.add('' +  ${interpolate(key, '') || 'l'})
       }
   `)
 
