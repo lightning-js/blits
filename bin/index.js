8fb8aea60832dcda3e724c7ad029f7795cf41b07
sandeep-vedam <sandeepsharma.vedam@outlook.com>
2024-10-01 00:38:56 +0530

    Updated the package name in bin index file

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@
 import { execSync } from 'child_process'
 
 try {
-  execSync('npx --yes blits-create@latest', { stdio: 'inherit' })
+  execSync('npx --yes create-blits@latest', { stdio: 'inherit' })
 } catch (error) {
   console.error('Error running blits-create:', error)
   process.exit(1)
