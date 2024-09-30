5bd4ffa5f1c041281f1fdd5fb631a0d77d00629a
sandeep-vedam <sandeepsharma.vedam@outlook.com>
2024-10-01 00:41:26 +0530

    Corrected the package name inside bin index file

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@
 import { execSync } from 'child_process'
 
 try {
-  execSync('npx --yes create-blits@latest', { stdio: 'inherit' })
+  execSync('npx --yes @lightningjs/create-blits@latest', { stdio: 'inherit' })
 } catch (error) {
   console.error('Error running blits-create:', error)
   process.exit(1)
