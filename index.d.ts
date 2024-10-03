2c00ffd113cb9a1290199bf993d934d0de8e0e55
Michiel van der Geest <hello@michielvandergeest.com>
2024-10-03 09:36:48 +0200

    Refactored index.d.ts. to work properly with this scope in methods.

--- index.d.ts
+++ index.d.ts
@@ -17,163 +17,106 @@
 
 declare module '@lightningjs/blits' {
 
-  /**
-   * Name of the Component
-   */
-  type ComponentName = string
-
-  type StateDefinition<S> = () => S;
-
-  type MethodsDefinition<M> = {
-      [K in keyof M]: M[K] extends (...args: any[]) => any ? M[K] : never;
-  };
-
-  type PropObject<T = any> = {
+  interface Hooks {
       /**
-       * Name of the prop
-       */
-      key: string;
+      * Fires when the Component is being instantiated
+      * At this moment child elements will not be available yet
+      */
+      init?: () => void;
       /**
-       * Whether the prop is required to be passed
-       */
-      required?: boolean,
+      * Fires when the Component is fully initialized and ready for interaction.
+      */
+      ready?: () => void;
       /**
-       * Default value for the prop when omited
-       */
-      default?: any,
+      * Triggers when the Component receives focus.
+      *
+      * This event can fire multiple times during the component's lifecycle
+      */
+      focus?: () => void;
       /**
-       * Cast the value of the prop
-       *
-       * @example
-       * ```js
-       * {
-       *   cast: Number, // casts to a number
-       *   cast: (v) => v.toUpperCase() // casts to uppercase
-       * }
-       * ```
-       */
-      cast?: (v: any) => any
-      type?: T;
-  };
+      * Triggers when the Component loses focus.
+      *
+      * This event can fire multiple times during the component's lifecycle
+      */
+      unfocus?: () => void;
+      /**
+      * Fires when the Component is being destroyed and removed.
+      */
+      destroy?: () => void;
+      /**
+      * Fires upon each frame start  (allowing you to tap directly into the renderloop)
+      *
+      * Note: This hook will fire continuously, multiple times per second!
+      */
+      frameTick?: () => void;
+      /**
+      * Fires when the component enters the viewport _margin_ and is attached to the render tree
+      *
+      * This event can fire multiple times during the component's lifecycle
+      */
+      attach?: () => void;
+      /**
+      * Fires when the component leaves the viewport _margin_ and is detached from the render tree
+      *
+      * This event can fire multiple times during the component's lifecycle
+      */
+      detach?: () => void;
+      /**
+      * Fires when the component enters the visible viewport
+      *
+      * This event can fire multiple times during the component's lifecycle
+      */
+      enter?: () => void;
+      /**
+      * Fires when the component leaves the visible viewport
+      *
+      * This event can fire multiple times during the component's lifecycle
+      */
+      exit?: () => void;
+  }
 
-  // Prop can be either a string or PropObject
-  type Prop = string | PropObject;
-
-  // Props array where each item can be a string or PropObject
-  type PropArray = Prop[];
-
-  // Extract the prop names from the props array
-  type ExtractPropNames<P extends PropArray> = {
-      readonly [K in P[number] as K extends string ? K : K extends { key: infer Key } ? Key : never]: any;
-  };
-
-  // PropsDefinition to handle props as strings or objects
-  type PropsDefinition<P extends PropArray> = ExtractPropNames<P>;
-
-  interface Input<S, M, P extends PropArray> {
-      [key: string]: ((this: ComponentContext<S, M, P>, event: KeyboardEvent) => void) | undefined,
+  interface Input {
+      [key: string]: (event: KeyboardEvent) => void | undefined,
       /**
        * Catch all input function
        *
        * Will be invoked when there is no dedicated function for a certain key
       */
-      any?: (this: ComponentContext<S, M, P>, event: KeyboardEvent) => void,
-  }
-  interface Methods<S, M, P extends PropArray> {
-      [key: string]: ((this: ComponentContext<S, M, P>, ...args: any[]) => void) | undefined,
+      // @ts-ignore
+      any?: (event: KeyboardEvent) => void
   }
 
-
-  // Combines the this scope of a Component consisting out of props, state, methods, and the component base
-  type ComponentContext<S, M, P extends PropArray> = PropsDefinition<P> & S & MethodsDefinition<M> & ComponentBase;
-
-  // Component base with built-in utility methods
   type ComponentBase = {
       /**
-       * Listen to events emitted by other components
+       * Emit events that other components can listen to
+       * @param name - name of the event to be emitted
+       * @param data - optional data to be passed along
        */
-      $listen: (event: string, callback: (args: any) => void) => void;
+      $emit(name: string, data?: any): void;
+  }
 
-      /**
-       * Emits an event.
-       * @param eventName - The name of the event.
-       * @param args - The callback function to handle the event.
-       */
-      $emit(eventName: string, args?: any): void;
-
-      /**
-       * Set a timeout that is automatically cleaned upon component destroy
-       */
-      $setTimeout: (callback: (args: any) => void, ms?: number | undefined) => ReturnType<typeof setTimeout>;
-
-      /**
-       * Clear a timeout
-       */
-      $clearTimeout: (id: ReturnType<typeof setTimeout>) => void;
-
-      /**
-       * Set an interval that is automatically cleaned upon component destroy
-       */
-      $setInterval: (callback: (args: any) => void, ms?: number | undefined) => ReturnType<typeof setInterval>
-
-      /**
-       * Clear a interval
-       */
-      $clearInterval: (id: ReturnType<typeof setInterval>) => void
-
-      /**
-       * Set focus to the Component, optionally pass a KeyboardEvent for instant event bubbling
-       */
-      $focus: (event?: KeyboardEvent) => void
-
-      /**
-       * @deprecated
-       * Deprecated: use `this.$select()` instead
-       */
-      focus: () => void
-
-      /**
-       * Select a child Element or Component by ref
-       *
-       * Elements and Components in the template can have an optional ref argument.
-       * Returns an Element Instance or Component Instance.
-       * Useful for passing on the focus to a Child component.
-       *
-       * @example
-       * ```js
-       * const menu = this.select('Menu')
-       * if(menu) {
-       *   menu.focus()
-       * }
-       * ```
-       */
-      $select: (ref: string) => any //ComponentInstance | ElementInstance
-
-      /**
-       * @deprecated
-       * Deprecated: use `this.$select()` instead
-       */
-      select: () => void
-
-      /**
-       * Triggers a forced update on state variables.
-       */
-      $trigger: (key: string) => void
-
-      /**
-       * @deprecated
-       * Deprecated: use `this.$trigger()` instead
-       */
-      trigger: () => void
-
+  /**
+   * Prop object
+   */
+  type PropObject<T = any> = {
+      key: string;
+      type?: T;
   };
 
-  // Component configuration object
-  type ComponentConfig<P extends PropArray, S, M> = {
-      /**
-       * Todo: components
-       */
-      components?: any;
+  // Props Array
+  type PropsArray = (string | PropObject)[];
+
+  // Extract the prop names from the props array
+  type ExtractPropNames<P extends PropsArray> = {
+      readonly [K in P[number] as K extends string ? K : K extends { key: infer Key } ? Key : never]: any;
+  };
+
+  // Update the PropsDefinition to handle props as strings or objects
+  type PropsDefinition<P extends PropsArray> = ExtractPropNames<P>;
+
+  type ComponentContext<P extends PropsArray, S, M, C> = ThisType<PropsDefinition<P> & S & M & C & ComponentBase>
+
+  type ComponentOptions<P extends PropsArray, S, M, C, W> = {
       /**
        * XML-based template string of the Component
        *
@@ -189,7 +132,7 @@ declare module '@lightningjs/blits' {
        * </Element>
        * ```
        */
-      template: string;
+      template?: String,
       /**
        * Allowed props to be passed into the Component by the parent
        *
@@ -226,224 +169,28 @@ declare module '@lightningjs/blits' {
        * }
        * ```
        */
-      state?: StateDefinition<S>;
+      state?: () => S;
       /**
-       * Todo: Computed properties
+       * Methods for abstracting more complex business logic into separate function
        */
-      computed?: any
-      /**
-       * Todo: Watchers for changes to state variables, props or computed properties
-       */
-      watch?: any
-      /**
-       * Methods for custom business logic
-       */
-      methods?: M; //Methods<S, M, P>;
+      methods?: M & ComponentContext<P, S, M, C>
       /**
        * Hooking into Lifecycle events
        */
-      hooks?: {
-          /**
-           * Fires when the Component is being instantiated.
-           * At this moment child elements will not be available yet
-           */
-          init?: (this: ComponentContext<S, M, P>) => void;
-          /**
-           * Fires when the Component is fully initialized and ready for interaction.
-           */
-          ready?: (this: ComponentContext<S, M, P>) => void;
-          /**
-           * Triggers when the Component receives focus.
-           *
-           * This event can fire multiple times during the component's lifecycle
-           */
-          focus?: (this: ComponentContext<S, M, P>) => void;
-          /**
-           * Triggers when the Component loses focus.
-           *
-           * This event can fire multiple times during the component's lifecycle
-           */
-          unfocus?: (this: ComponentContext<S, M, P>) => void;
-          /**
-           * Fires when the Component is being destroyed and removed.
-           */
-          destroy?: (this: ComponentContext<S, M, P>) => void;
-          /**
-           * Fires upon each frame start  (allowing you to tap directly into the renderloop)
-           *
-           * Note: This hook will fire continuously, multiple times per second!
-           */
-          frameTick?: (this: ComponentContext<S, M, P>) => void;
-          /**
-           * Fires when the component enters the viewport _margin_ and is attached to the render tree
-           *
-           * This event can fire multiple times during the component's lifecycle
-           */
-          attach?: (this: ComponentContext<S, M, P>) => void;
-          /**
-           * Fires when the component leaves the viewport _margin_ and is detached from the render tree
-           *
-           * This event can fire multiple times during the component's lifecycle
-           */
-          detach?: (this: ComponentContext<S, M, P>) => void;
-          /**
-           * Fires when the component enters the visible viewport
-           *
-           * This event can fire multiple times during the component's lifecycle
-           */
-          enter?: (this: ComponentContext<S, M, P>) => void;
-          /**
-           * Fires when the component leaves the visible viewport
-           *
-           * This event can fire multiple times during the component's lifecycle
-           */
-          exit?: (this: ComponentContext<S, M, P>) => void;
-        };
+      hooks?: Hooks & ComponentContext<P, S, M, C>
       /**
-       * Handling user input
+       * Tapping into user input
        */
-      input?: Input<S, M, P>
-  };
-
-  interface ApplicationOptions<P extends PropArray, S, M> extends ComponentConfig<P, S, M> {
-    /**
-     * Routes definition
-     *
-     * @example
-     *
-     * ```js
-     * routes: [
-     *  { path: '/', component: Home },
-     *  { path: '/details', component: Details },
-     *  { path: '/account', component: Account },
-     * ]
-     * ```
-     */
-    routes?: Route[]
+      input?: Input & ComponentContext<P, S, M, C>
+      /**
+       * Computed properties
+       */
+      computed?: C & ComponentContext<P, S, M, C>
+      /**
+       * Watchers for changes to state variables, props or computed properties
+       */
+      watch?: W & ComponentContext<P, S, M, C>
   }
-
-  interface Transition {
-    /**
-     * Name of the prop to transition (i.e. 'x', 'y', 'alpha', 'color')
-     */
-    prop: string,
-    /**
-     * Value the prop should transition to (i.e. 0, 100, '#223388')
-     */
-    value: any,
-    /**
-     * Duration of the transition in milliseconds, defaults to 300
-     */
-    duration?: number,
-    /**
-     * Easing function to apply to the transition
-     */
-    easing?: string,
-    /**
-     * Delay before the transition starts in milliseconds
-     */
-    delay?: number
-  }
-
-  interface Before {
-    /**
-    * Name of the prop to set before the transition starts
-    */
-    prop: string,
-    /**
-     * Value the prop to set before the transition starts
-     */
-    value: any,
-  }
-
-  interface RouteTransition {
-    /**
-     * Setting or Array of Settings before new view enters into the router view
-     */
-    before: Before | Before[],
-    /**
-     * Transition or Array of Transitions for new view to enters into the router view
-     */
-    in: Transition | Transition[],
-    /**
-     * Transition or Array of Transitions for old view to leave the router view
-     */
-    out: Transition | Transition[],
-  }
-
-  type RouteTransitionFunction = (previousRoute: Route, currentRoute: Route) => RequireAtLeastOne<RouteTransition>
-
-  interface RouteAnnounce {
-    /**
-     * Message to be announced
-     */
-    message: String,
-    /**
-     * Politeness level
-     *
-     * Defaults to 'off'
-     */
-    politeness?: 'off' | 'polite' | 'assertive'
-  }
-
-  type RequireAtLeastOne<T> = {
-    [K in keyof T]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<keyof T, K>>>
-  }[keyof T]
-
-  interface RouteHooks {
-    before?: (to: Route, from: Route) => string | Route;
-  }
-
-  type Route = {
-    /**
-     * URI path for the route
-     */
-    path: string,
-    /**
-     * Component to load when activating the route
-     */
-    component: Component | (() => Promise<Component>),
-    /**
-     * Transition configuration for the route
-     */
-    transition?: RequireAtLeastOne<RouteTransition> | RouteTransitionFunction,
-    /**
-     * Extra route options
-     */
-    options?: object // todo: specify which options are available,
-    /**
-     * Message to be announced when visiting the route (often used for accessibility purposes)
-     *
-     * Can be either a `String` with the message or an object that defines the message and the
-     * politeness level
-     */
-    announce?: String | RouteAnnounce
-    /**
-     * Register hooks for the route
-     */
-    hooks?: RouteHooks
-    /**
-     * Route path parameters
-     */
-    readonly params?: {
-      [key: string]: string | number
-    }
-    /**
-     * Allows for attaching custom data to a route, either hardcoded in the
-     * route definition or asigned to the route object in a before hook
-     *
-     * Will be merged with the route params and navigation data and passed as
-     * props into the route component
-     */
-    data?: {
-      [key: string]: any
-    }
-  }
-
-  type Component = <P extends PropArray, S, M extends Methods<S, M, P>>(
-    name: ComponentName,
-    options: ComponentConfig<P, S, M>
-) => Component
 
   /**
    * Blits App Framework
@@ -452,17 +199,22 @@ declare module '@lightningjs/blits' {
       /**
        * Blits Application
        */
-      Application: <P extends PropArray, S, M extends Methods<S, M, P>>(
-        options: ApplicationOptions<P, S, M>
-      ) => {};
-      /**
+      Application() : void
+       /**
        * Blits Component
        */
-      Component: Component;
+      Component<P extends PropsArray, S, M, C, W>(name: string, options: ComponentOptions<P, S, M, C, W>) : void
+      /**
+       * Blits Launch
+       */
+      Launch() : void
+      /**
+       * Blits Plugin
+       */
+      Plugin() : void
   }
 
   const Blits: Blits;
 
   export default Blits;
-
 }
