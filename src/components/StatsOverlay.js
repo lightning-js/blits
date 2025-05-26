bc7d073b6bdabe55a548668c7d43f8ab62ec9980
wouterlucas <wouter@wouterlucas.com>
2025-05-26 13:03:48 +0200

    fix: update StatsOverlay to conditionally render based on BLITS_STATS_ENABLED

--- src/components/StatsOverlay.js
+++ src/components/StatsOverlay.js
@@ -26,113 +26,117 @@ import { Log } from '../lib/log.js'
  * Only displayed when BLITS_STATS_ENABLED is true
  */
 export default () =>
-  BLITS_STATS_ENABLED
-    ? Component('StatsOverlay', {
-        template: `
-          <Element>
-            <Element x="5" y="5" color="black">
-              <Text :content="$statsTitle" fontSize="22" fontWeight="bold" color="#fff" />
-              <Element y="30">
-                <Text :content="$componentStats" fontSize="18" color="#fff" />
-              </Element>
-              <Element y="55">
-                <Text :content="$elementStats" fontSize="18" color="#fff" />
-              </Element>
-              <Element y="80">
-                <Text :content="$listenerStats" fontSize="18" color="#fff" />
-              </Element>
-              <Element y="105">
-                <Text :content="$timeoutStats" fontSize="18" color="#fff" />
-              </Element>
-              <Element y="130">
-                <Text :content="$intervalStats" fontSize="18" color="#fff" />
-              </Element>
-              <Element y="165" :visible="$hasMemoryInfo">
-                <Text :content="$memoryTitle" fontSize="22" fontWeight="bold" color="#fff" />
-              </Element>
-              <Element y="195" :visible="$hasMemoryInfo">
-                <Text :content="$memoryStats1" fontSize="18" color="#fff" />
-              </Element>
-              <Element y="220" :visible="$hasMemoryInfo">
-                <Text :content="$memoryStats2" fontSize="18" color="#fff" />
-              </Element>
-            </Element>
+  Component('StatsOverlay', {
+    template: `
+      <Element :visible="$statsEnabled">
+        <Element x="5" y="5" color="black">
+          <Text :content="$statsTitle" fontSize="22" fontWeight="bold" color="#fff" />
+          <Element y="30">
+            <Text :content="$componentStats" fontSize="18" color="#fff" />
           </Element>
-        `,
-        state() {
-          return {
-            statsTitle: 'System Statistics',
-            componentStats: 'Components: Loading...',
-            elementStats: 'Elements: Loading...',
-            listenerStats: 'Listeners: Loading...',
-            timeoutStats: 'Timeouts: Loading...',
-            intervalStats: 'Intervals: Loading...',
+          <Element y="55">
+            <Text :content="$elementStats" fontSize="18" color="#fff" />
+          </Element>
+          <Element y="80">
+            <Text :content="$listenerStats" fontSize="18" color="#fff" />
+          </Element>
+          <Element y="105">
+            <Text :content="$timeoutStats" fontSize="18" color="#fff" />
+          </Element>
+          <Element y="130">
+            <Text :content="$intervalStats" fontSize="18" color="#fff" />
+          </Element>
+          <Element y="165" :visible="$hasMemoryInfo">
+            <Text :content="$memoryTitle" fontSize="22" fontWeight="bold" color="#fff" />
+          </Element>
+          <Element y="195" :visible="$hasMemoryInfo">
+            <Text :content="$memoryStats1" fontSize="18" color="#fff" />
+          </Element>
+          <Element y="220" :visible="$hasMemoryInfo">
+            <Text :content="$memoryStats2" fontSize="18" color="#fff" />
+          </Element>
+        </Element>
+      </Element>
+    `,
+    state() {
+      return {
+        statsEnabled: BLITS_STATS_ENABLED,
+        statsTitle: 'System Statistics',
+        componentStats: 'Components: Loading...',
+        elementStats: 'Elements: Loading...',
+        listenerStats: 'Listeners: Loading...',
+        timeoutStats: 'Timeouts: Loading...',
+        intervalStats: 'Intervals: Loading...',
 
-            hasMemoryInfo: false,
-            memoryTitle: 'Renderer Memory',
-            memoryStats1: '',
-            memoryStats2: '',
+        hasMemoryInfo: false,
+        memoryTitle: 'Renderer Memory',
+        memoryStats1: '',
+        memoryStats2: '',
+      }
+    },
+    hooks: {
+      ready() {
+        // Only start the interval if stats are enabled
+        if (BLITS_STATS_ENABLED) {
+          Log.info('BlitsStatsOverlay: Starting stats update interval')
+          this._interval = this.$setInterval(() => {
+            this.updateStats()
+          }, 500)
+        }
+      },
+      destroy() {
+        if (this._interval) {
+          this.$clearInterval(this._interval)
+        }
+      },
+    },
+    methods: {
+      updateStats() {
+        // Skip if stats are disabled
+        if (!BLITS_STATS_ENABLED) return
+
+        // Update system stats
+        const components = getStats('components')
+        const elements = getStats('elements')
+        const listeners = getStats('eventListeners')
+        const timeouts = getStats('timeouts')
+        const intervals = getStats('intervals')
+
+        if (components) {
+          this.componentStats = `Components: Active ${components.active} | Created ${components.created} | Deleted ${components.deleted}`
+        }
+
+        if (elements) {
+          this.elementStats = `Elements: Active ${elements.active} | Created ${elements.created} | Deleted ${elements.deleted}`
+        }
+
+        if (listeners) {
+          this.listenerStats = `Listeners: Active ${listeners.active} | Created ${listeners.created} | Deleted ${listeners.deleted}`
+        }
+
+        if (timeouts) {
+          this.timeoutStats = `Timeouts: Active ${timeouts.active} | Created ${timeouts.created} | Deleted ${timeouts.deleted}`
+        }
+
+        if (intervals) {
+          this.intervalStats = `Intervals: Active ${intervals.active} | Created ${intervals.created} | Deleted ${intervals.deleted}`
+        }
+
+        // Update memory info if available
+        const memInfo = renderer?.stage?.txMemManager.getMemoryInfo() || null
+        if (memInfo) {
+          this.hasMemoryInfo = true
+          this.memoryStats1 = `Memory: ${bytesToMb(memInfo.memUsed)}MB | Renderable: ${bytesToMb(
+            memInfo.renderableMemUsed
+          )}MB`
+          this.memoryStats2 = `Target: ${bytesToMb(
+            memInfo.targetThreshold
+          )}MB | Critical: ${bytesToMb(memInfo.criticalThreshold)}MB`
+
+          if (memInfo.loadedTextures !== undefined) {
+            this.memoryStats2 += ` | Textures: ${memInfo.loadedTextures}`
           }
-        },
-        hooks: {
-          ready() {
-            // Update stats every 500ms
-            Log.info('BlitsStatsOverlay: Starting stats update interval')
-            this._interval = setInterval(() => {
-              this.updateStats()
-            }, 500)
-          },
-          destroy() {
-            if (this._interval) {
-              clearInterval(this._interval)
-            }
-          },
-        },
-        methods: {
-          updateStats() {
-            // Update system stats
-            const components = getStats('components')
-            const elements = getStats('elements')
-            const listeners = getStats('eventListeners')
-            const timeouts = getStats('timeouts')
-            const intervals = getStats('intervals')
-
-            if (components) {
-              this.componentStats = `Components: Active ${components.active} | Created ${components.created} | Deleted ${components.deleted}`
-            }
-
-            if (elements) {
-              this.elementStats = `Elements: Active ${elements.active} | Created ${elements.created} | Deleted ${elements.deleted}`
-            }
-
-            if (listeners) {
-              this.listenerStats = `Listeners: Active ${listeners.active} | Created ${listeners.created} | Deleted ${listeners.deleted}`
-            }
-
-            if (timeouts) {
-              this.timeoutStats = `Timeouts: Active ${timeouts.active} | Created ${timeouts.created} | Deleted ${timeouts.deleted}`
-            }
-
-            if (intervals) {
-              this.intervalStats = `Intervals: Active ${intervals.active} | Created ${intervals.created} | Deleted ${intervals.deleted}`
-            }
-
-            // Update memory info if available
-            const memInfo = renderer?.stage?.txMemManager.getMemoryInfo() || null
-            if (memInfo) {
-              this.hasMemoryInfo = true
-              this.memoryStats1 = `Memory: ${bytesToMb(
-                memInfo.memUsed
-              )}MB | Renderable: ${bytesToMb(memInfo.renderableMemUsed)}MB`
-              this.memoryStats2 = `Target: ${bytesToMb(
-                memInfo.targetThreshold
-              )}MB | Critical: ${bytesToMb(memInfo.criticalThreshold)}MB`
-
-              if (memInfo.loadedTextures !== undefined) {
-                this.memoryStats2 += ` | Textures: ${memInfo.loadedTextures}`
-              }
-            }
-          },
-        },
-      })
-    : () => null
+        }
+      },
+    },
+  })
