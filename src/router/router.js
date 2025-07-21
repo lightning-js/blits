d4d8c49b1c45025585d7dc2a2fa0245e78aa6e3e
Michiel van der Geest <hello@michielvandergeest.com>
2025-07-21 10:47:39 +0200

    Fixed issue with router throwing an error when a route doesn't exist instead of handling it gracefully.

--- src/router/router.js
+++ src/router/router.js
@@ -132,7 +132,7 @@ export const matchHash = (path, routes = []) => {
   let matchingRoute = false
   let i = 0
   while (!matchingRoute && i < routes.length) {
-    const route = { ...routes[i] }
+    const route = routes[i]
     route.path = normalizePath(route.path)
     if (route.path === path) {
       route.params = {}
@@ -206,20 +206,20 @@ export const navigate = async function () {
     let previousRoute = currentRoute //? Object.assign({}, currentRoute) : undefined
     const { hash, path, queryParams } = getHash()
     let route = matchHash(path, this.parent[symbols.routes])
-    const queryParamsData = {}
-    const queryParamsEntries = [...queryParams.entries()]
-    for (let i = 0; i < queryParamsEntries.length; i++) {
-      queryParamsData[queryParamsEntries[i][0]] = queryParamsEntries[i][1]
-    }
-
-    route.data = {
-      ...route.data,
-      ...navigationData,
-      ...queryParamsData,
-    }
 
     currentRoute = route
     if (route) {
+      const queryParamsData = {}
+      const queryParamsEntries = [...queryParams.entries()]
+      for (let i = 0; i < queryParamsEntries.length; i++) {
+        queryParamsData[queryParamsEntries[i][0]] = queryParamsEntries[i][1]
+      }
+
+      route.data = {
+        ...navigationData,
+        ...route.data,
+        ...queryParamsData,
+      }
       // Adding the location hash to the route if it exists.
       if (hash !== null) {
         route.hash = hash
