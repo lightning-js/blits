510cdba11dfc6f828c35478f492b939f883412ce
Michiel van der Geest <hello@michielvandergeest.com>
2025-08-05 07:45:56 +0200

    Changed effects attribute to rounded attribute for built-in circle component.

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -20,7 +20,7 @@ import Component from '../component.js'
 export default () =>
   Component('Circle', {
     template: `
-      <Element :color="$color" :w="$size" :h="$size" :effects="[$shader('radius', {radius: $radius})]"></Element>
+      <Element :color="$color" :w="$size" :h="$size" shader="{type: 'rounded', {radius: $radius}}"></Element>
     `,
     props: [{ key: 'size', default: 40 }, 'color'],
     computed: {
