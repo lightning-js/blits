db798292e13c066c284d3c46c83f56a1fce392e7
Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>
2025-09-09 17:41:47 +0530

    Assign null to router state.data
    Assigning null to router state data property before assigning
    actual route data. This will make sure not triggering reactivity effects
    of previous route component
    
    Signed-off-by: Suresh Kumar Gangumalla <sureshkumar.gangumalla@invecas.com>

--- src/router/router.js
+++ src/router/router.js
@@ -320,6 +320,7 @@ export const navigate = async function () {
       state.path = route.path
       state.params = route.params || {}
       state.hash = route.hash
+      state.data = null
       state.data = route.data || {}
 
       if (!view) {
