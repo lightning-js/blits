994c90322df9aeb7fb9db1675bed217c67969523
Michiel van der Geest <hello@michielvandergeest.com>
2025-11-13 12:23:08 +0100

    Updated arguments of fpsUpdate and frameTick in type definitions.

--- index.d.ts
+++ index.d.ts
@@ -169,9 +169,9 @@ declare module '@lightningjs/blits' {
     *
     * Note: This hook will fire continuously, multiple times per second!
     */
-    frameTick?: () => void;
+    frameTick?: (data: {time: number, delta: number}) => void;
     /**
-    * Fires when the component enters the viewport _margin_ and is attached to the render tree
+    * Fires when the component entes the viewport _margin_ and is attached to the render tree
     *
     * This event can fire multiple times during the component's lifecycle
     */
@@ -205,7 +205,7 @@ declare module '@lightningjs/blits' {
     *
     * Note: This event fire multiple times
     */
-    fpsUpdate?: () => void;
+    fpsUpdate?: (fps: number) => void;
   }
 
   export interface Input {
