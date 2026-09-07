f4af4425c7f671cc1bd70798cf63f8c5a27a441d
il-sairamg <SairamG@ideyalabs.com>
2026-09-07 03:36:50 -0700

    implement rich-text prop in Text component

--- src/engines/L3/element.js
+++ src/engines/L3/element.js
@@ -240,6 +240,9 @@ const propsTransformer = {
   set rotation(v) {
     this.props['rotation'] = v * (Math.PI / 180)
   },
+  set richtext(v) {
+    this.props['richText'] = v === true || v === 'true'
+  },
   set w(v) {
     const parsed = parsePercentage.call(this, v, 'w')
     this.props['w'] = parsed
